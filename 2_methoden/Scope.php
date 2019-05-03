ab8701b33e4a118da4edf8c3c78a37ca8088d6aa
Murat6161 <muratyavuz@gmx.de>
2019-05-04 00:27:33 +0200

    Die  Klasse Scope Hinzugefügt

---
+++ 2_methoden/Scope.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+class Scope {
+
+    public $durchschnittsAlter = 42;
+
+    public function differenzBerechnen ($meinAlter) {
+        $xyz = 5;
+    }
+
+}
+
+$objekt = new Scope();
+
+/*
+function addieren() {
+    $x = 5;
+}
+echo $x;
+ */
