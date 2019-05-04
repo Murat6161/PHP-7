9415cf9a0ad6b9f52ea90a42003e5950d8650f77
Murat6161 <muratyavuz@gmx.de>
2019-05-04 02:01:33 +0200

    Uebungsaufgabe 2 und Lösung

---
+++ 2_methoden/Loesung2.php
@@ -0,0 +1,48 @@
+<?php
+
+
+/*
+
+Die neue Lösung soll jeden Verleih erfassen:
+
+- Uhrzeit
+- Bootsnummer
+- Name der Person
+
+Schaffe ein oder mehrere geeignete(n) Klasse(n), über
+die dies abgebildet werden kann. Die Speicherung der Daten ist nicht
+notwendig. Es kann auch mit Dummywerten gearbeitet werden.
+
+*/
+
+class Bootsverleih {
+    const STUNDENSATZ = 4;
+
+    public $ausleihe = array();
+
+    public function bootLeihen($name, $bootNummer) {
+        echo "Verleih:<br>";
+
+        $uhrzeit = date("H:m");
+
+        $this->ausleihe[$bootNummer] = array("name" => $name, "start" => $uhrzeit);
+        printf("- Boot #%d eingebucht um %s Uhr<br>", $bootNummer, $uhrzeit);
+    }
+
+    public function bootAbgeben($bootNummer) {
+        $ausleihe = $this->ausleihe[$bootNummer];
+        $this->ausleihe[$bootNummer] = null;
+
+        // Dauer berechnen... $ausleihe["start"]
+        $dauer = 2;
+        $kosten = $dauer * self::STUNDENSATZ;
+
+        echo "Rückgabe:<br>";
+        echo "- Dauer: $dauer Stunden<br>";
+        echo "- Kosten: $kosten EUR";
+    }
+}
+
+$verleih = new Bootsverleih();
+$verleih->bootLeihen("Max", 61);
+$verleih->bootAbgeben(61);
